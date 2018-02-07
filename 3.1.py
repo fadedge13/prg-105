46d39f5f1811bd6ad09b04cda32ddb418f57e2a2
fadedge13 <35938908+fadedge13@users.noreply.github.com>
2018-02-07 17:26:17 -0600

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,20 @@
+# figure out which day of the week it is
+
+date = int(input("What day of the week is it?"))
+
+if date == 1:
+    print("Monday")
+elif date == 2:
+    print("Tuesday")
+elif date == 3:
+    print("Wednesday")
+elif date == 4:
+    print("Thursday")
+elif date == 5:
+    print("Friday")
+elif date == 6:
+    print("Saturday")
+elif date == 7:
+    print("Sunday")
+else:
+    print("incorrect value")
