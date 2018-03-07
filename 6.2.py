136d2886405b4e34b060cd97342d9a3b24fc1860
fadedge13 <35938908+fadedge13@users.noreply.github.com>
2018-03-07 17:07:31 -0600

    Add files via upload

---
+++ 6.2.py
@@ -0,0 +1,15 @@
+
+def main():
+	input_file = open('numbers.txt', 'r')
+	total_sum = 0
+	count = 0
+	for line in input_file:
+		count += 1
+		record = float(line)
+		total_sum += record
+
+	input_file.close()
+	ave = total_sum/count
+	print"There were a total of", str(count), "numbers"
+	print'The average of the numbers was', format(ave, '.2f')
+main()
