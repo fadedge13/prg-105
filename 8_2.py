5aa2e17a5a8854a84c00f26f5b64577b85714f3f
fadedge13 <35938908+fadedge13@users.noreply.github.com>
2018-04-23 17:29:38 -0500

    Update 8_2.py

--- 8_2.py
+++ 8_2.py
@@ -1,24 +1,27 @@
 def main():
     phoneNumb = input('Enter the number in the format of 555-XXX-XXXX\n')
-    phoneNumb = phoneNumb.split('-')
+    newPhoneNumb = ''
+    for var in phoneNumb:
+        if var.isalpha():
+            var = var.upper()
+            if var == 'A' or var == 'B' or var == 'C':
+                newPhoneNumb += '2'
+            elif var == 'D' or var == 'E' or var == 'F':
+                newPhoneNumb += '3'
+            elif var == 'G' or var == 'H' or var == 'I':
+                newPhoneNumb += '4'
+            elif var == 'J' or var == 'K' or var == 'L':
+                newPhoneNumb += '5'
+            elif var == 'M' or var == 'N' or var == 'O':
+                newPhoneNumb += '6'
+            elif var == 'P' or var == 'Q' or var == 'R':
+                newPhoneNumb += '7'
+            elif var == 'S' or var == 'T' or var == 'U':
+                newPhoneNumb += '8'
+            elif var == 'V' or var == 'W' or var == 'X' or var == 'Z':
+                newPhoneNumb += '9'
+        else:
+            newPhoneNumb += str(var)
 
-    for var in phoneNumb[1:2]:
-        for character in var:
-            if character == 'A' or character == 'B' or character == 'C':
-                character = '2'
-            elif character == 'D' or character == 'E' or character == 'F':
-                character = '3'
-            elif character == 'G' or character == 'H' or character == 'I':
-                character = '4'
-            elif character == 'J' or character == 'K' or character == 'L':
-                character = '5'
-            elif character == 'M' or character == 'N' or character == 'O':
-                character = '6'
-            elif character == 'P' or character == 'Q' or character == 'R':
-                character = '7'
-            elif character == 'S' or character == 'T' or character == 'U':
-                character = '8'
-            elif character == 'V' or character == 'W' or character == 'X' or character == 'Z':
-                character = '9'
-    print(phoneNumb)
+    print(newPhoneNumb)
 main()
